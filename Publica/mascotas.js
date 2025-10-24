f3fd6dd832155ffc372f6cf0af458436c679aa0c
Dario Di Gulio <DGDarioGabriel@proveedor.teco.com.ar>
2025-10-24 15:15:33 -0300

    Agrega loader al llamado de mascotas

--- Publica/mascotas.js
+++ Publica/mascotas.js
@@ -1,30 +1,26 @@
 async function mostrarMascotas() {
-    const res = await fetch('https://misterio07.alwaysdata.net/mascotas' );
-    console.log(res);
-    
-    mascotas= await res.json() 
-    //const mascotas =data.payload || [];
-    console.log(mascotas)
-
+    const res = await fetch('https://misterio07.alwaysdata.net/mascotas');
+    const mascotas = await res.json()
     const container = document.getElementById('container');
-    container. innerHTML = '';
-    
-    mascotas.forEach((mascota)=> {
-        console.log(mascotas)
+    container.innerHTML = '';
+    mascotas.forEach((mascota) => {
         let clon = document.querySelector('template').content.cloneNode(true)
         clon.querySelector('img').setAttribute("src", mascota.thumbnail)
-       clon.querySelector('h3').innerText = mascota.nombre;
-clon.querySelectorAll('p')[0].innerText += mascota.tipo;
-clon.querySelectorAll('p')[1].innerText += mascota.raza;
-clon.querySelectorAll('p')[2].innerText += mascota.edad;
-clon.querySelectorAll('p')[3].innerText += mascota.nombre;
-clon.querySelectorAll('p')[4].innerText += mascota.direccion;
-clon.querySelectorAll('p')[5].innerText += mascota.propietario;
+        clon.querySelector('h3').innerText = mascota.nombre;
+        clon.querySelectorAll('p')[0].innerText += mascota.tipo;
+        clon.querySelectorAll('p')[1].innerText += mascota.raza;
+        clon.querySelectorAll('p')[2].innerText += mascota.edad;
+        clon.querySelectorAll('p')[3].innerText += mascota.nombre;
+        clon.querySelectorAll('p')[4].innerText += mascota.direccion;
+        clon.querySelectorAll('p')[5].innerText += mascota.propietario;
         container.appendChild(clon)
-        
-    } )
-   // console.log(mascotas)
-   
+
+    })
 }
 
-mostrarMascotas()
+(async () => {
+    const loader = document.querySelector('.loader');
+    loader.style.display = 'block';
+    await mostrarMascotas()
+    loader.style.display = 'none';
+})();
